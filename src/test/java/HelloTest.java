349bf1f5750c825bc805217390a2bed9f5998b11
marat-karamian <marat_karamian@epam.com>
2018-07-22 23:10:40 +0300

    pom.xml added

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloTest {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
